c36fe81fa3d68b08954fa9d2beee31ba0184a083
liuzhaoyang <liuzhaoyang@maoyan.com>
2022-06-19 14:40:44 +0800

    initial

--- project/vc-3/src/main.ts
+++ project/vc-3/src/main.ts
@@ -2,7 +2,6 @@ import { createApp, h } from "vue";
 import App from "./App.vue";
 // import './registerServiceWorker'
 import router from "./router";
-console.log(12312);
 const app = createApp({
   render: () => h(App),
 });
