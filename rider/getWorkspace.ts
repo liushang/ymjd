51cb951c2ac28f3f12c871242d505291d5a848ca
liuzhaoyang <liuzhaoyang@maoyan.com>
2022-06-29 22:00:43 +0800

    1

--- rider/getWorkspace.ts
+++ rider/getWorkspace.ts
@@ -2,6 +2,8 @@ import path from 'path'
 import { getPackages } from '@manypkg/get-packages'
 
 export const getWorkspace = async () => {
+  console.log('bere')
   const wk = await getPackages(path.resolve(__dirname, '../'))
+  console.log(wk)
   return wk
 }
